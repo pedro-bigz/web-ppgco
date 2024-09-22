a9301e00d03f498f6e6484a41eb648b6f7452816
Pedro Big Z <pedropaulo.spaiva.a@gmail.com>
2024-09-22 02:25:30 -0300

    mend

--- src/components/Listing/Table/ActionsDropdown.tsx
+++ src/components/Listing/Table/ActionsDropdown.tsx
@@ -64,7 +64,7 @@ export function ActionsDropdown({
   return (
     <div
       ref={containerRef}
-      className="flex w-[130px]"
+      className="flex min-h-[56px] w-[130px]"
       style={{ height: dropdownHeight }}
     >
       <div
--- src/components/Listing/Table/Table.tsx
+++ src/components/Listing/Table/Table.tsx
@@ -73,7 +73,6 @@ export function Table({
     isRemoteData,
     onSelectKeys,
   });
-  console.log({ rows });
 
   return (
     <NextTable
